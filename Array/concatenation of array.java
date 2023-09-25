451285eb6abb144c79c5a2ec28d34af3b15edf49
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-25 12:19:58 +0500

    concatenation of array.java

---
+++ Array/concatenation of array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[2*n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+        }
+        int sec = ans.length;
+        int k = 0;
+        for(int i = n; i < sec; i++) {
+            ans[i] = nums[k];
+            k++; 
+        }
+        return ans;
+    }
+}
