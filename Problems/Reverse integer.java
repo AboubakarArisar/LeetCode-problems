6c6296f5127915ea015d18d4b5f14304b9da3da1
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-25 11:51:54 +0500

    Reverse integer.java

---
+++ Problems/Reverse integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+
+            // Check for integer overflow before updating the reversed integer
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && rem > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && rem < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
