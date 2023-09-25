f6f286e7818ac499741981fb10ebaee79978119b
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-25 12:48:49 +0500

    Value of variable.java
    Intuition and Solution for Final Value of Variable After Operations
    Intuition
    The problem involves determining the final value of a variable after applying a series of increment and decrement operations. The initial value of the variable is 0, and operations are provided in string format.
    
    Approach
    The provided code defines a Solution class with a method finalValueAfterOperations that iterates through the given operations and updates the variable based on the operation type. It follows the rules:
    
    "X++": Increment the variable x by 1.
    "++X": Increment the variable x by 1.
    "X--": Decrement the variable x by 1.
    "--X": Decrement the variable x by 1.
    The method returns the final value of the variable x after applying all operations.
    
    Complexity
    Time complexity: 
    �
    (
    �
    )
    O(n), where n is the number of operations in the input array. The algorithm iterates through each operation once.
    Space complexity: 
    �
    (
    1
    )
    O(1), as no extra space is used that scales with the input. The algorithm uses a constant amount of space for the integer variable x.

---
+++ Array/Value of variable.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("X++")) {
+                x++;
+            } else if (operations[i].equals("++X")) {
+                ++x;
+            } else if (operations[i].equals("X--")) {
+                x--;
+            } else if (operations[i].equals("--X")) {
+                --x;
+            }
+        }
+
+        return x;
+    }
+}
