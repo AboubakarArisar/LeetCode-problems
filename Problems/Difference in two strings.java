bf21afe2b12d3da85dfa8ad73d2aa8a800dcc3f5
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-09-25 11:37:14 +0500

    Difference in two strings.java
    389. Find the Difference
    Solved
    Easy
    Topics
    Companies
    You are given two strings s and t.
    
    String t is generated by random shuffling string s and then add one more letter at a random position.
    
    Return the letter that was added to t.
    
     
    
    Example 1:
    
    Input: s = "abcd", t = "abcde"
    Output: "e"
    Explanation: 'e' is the letter that was added.
    Example 2:
    
    Input: s = "", t = "y"
    Output: "y"
     
    
    Constraints:
    
    0 <= s.length <= 1000
    t.length == s.length + 1
    s and t consist of lowercase English letters.

---
+++ Problems/Difference in two strings.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char found = 0;
+
+        int size = s.length();
+
+        for (int i = 0; i < size; i++) {
+            boolean charFound = false;
+
+            for (int j = 0; j < t.length(); j++) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    charFound = true;
+                    break;
+                }
+            }
+
+            if (!charFound) {
+                found = s.charAt(i);
+                break;
+            }
+        }
+
+        if (found == 0) {
+            found = t.charAt(size - 1); // Corrected the index to size - 1
+        }
+
+        return found;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwu";
+        String t = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhky";
+        System.out.println("Output: " + solution.findTheDifference(s, t));  // Print the output
+    }
+}
