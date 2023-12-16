1fc26e679d05416cd62160950a28f1881ffa205b
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-16 19:36:28 +0500

    Number of Cells

---
+++ Queues/NumberofCells.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class RecentCounter {
+    private Queue<Integer> requests;
+
+    public RecentCounter() {
+        requests = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        requests.add(t);
+
+        while (!requests.isEmpty() && requests.peek() < t - 3000) {
+            requests.poll();
+        }
+
+        return requests.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
