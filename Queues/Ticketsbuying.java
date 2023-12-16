ccc7563bf760021541457b92ccd3a242e0e990d5
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-17 02:45:38 +0500

    Create Ticketsbuying.java

---
+++ Queues/Ticketsbuying.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        while(tickets[k] != 0){
+            for(int i = 0 ; i < tickets.length ; i++){
+                if(tickets[i] != 0 && tickets[k] != 0){
+                    tickets[i] --;
+                    time ++;
+                }
+            }
+        }
+
+        return time;
+    }
+}
