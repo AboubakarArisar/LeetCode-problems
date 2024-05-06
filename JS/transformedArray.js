e320a7d6a467ae4c503ce48520cbdddf207edbf2
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-05-06 17:26:56 +0500

    transformed array in js question on leetcode

---
+++ JS/transformedArray.js
@@ -0,0 +1,7 @@
+var map = function(arr, fn) {
+  const transformedArr = [];
+  arr.forEach((element, index) => {
+    transformedArr[index] = fn(element, index);
+  });
+  return transformedArr;
+};
