5c8f4a59709c081d26df1122d9bc2babad83bf27
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-07-18 00:02:34 +0500

    best time to buy sell problem array python

---
+++ Array/buysell.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        
+        return max_profit
